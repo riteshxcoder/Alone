b794843ea5c4b7b5f48e1d992482f10c76471256
- ♔ 𝐒 𝐇 𝐈 𝐕 𝐀 𝐍 𝐆 🜲 ˹ 𝐎ᴘ ˼﹛🇨🇦﹜≈ 💸 <Shivmis165@gmail.com>
2025-10-15 08:00:07 +0530

    Update ndatabase.py

--- AloneMusic/utils/ndatabase.py
+++ AloneMusic/utils/ndatabase.py
@@ -72,7 +72,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from AnieXEricaMusic.core.userbot import assistants
+    from AloneMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -86,7 +86,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from AnieXEricaMusic.core.userbot import assistants
+    from AloneMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -113,7 +113,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from AnieXEricaMusic.core.userbot import assistants
+    from AloneMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -126,7 +126,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from AnieXEricaMusic.core.userbot import assistants
+    from AloneMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
