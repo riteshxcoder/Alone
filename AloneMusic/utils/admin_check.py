fbaca5e0ede5ab6184914c15faa599e74db9b951
riteshxcoder <riteshxcoder@users.noreply.github.com>
2025-11-08 04:17:12 +0000

    Formate By @TheAlonetean
    Signed-off-by: TheAloneTeam <TheAloneTeam@users.noreply.github.com>

--- AloneMusic/utils/admin_check.py
+++ AloneMusic/utils/admin_check.py
@@ -1,6 +1,7 @@
 from pyrogram.types import CallbackQuery
 from pyrogram.enums import ChatType, ChatMemberStatus
 
+
 async def is_admin(message_or_cq) -> bool:
     if isinstance(message_or_cq, CallbackQuery):
         message = message_or_cq.message
@@ -19,6 +20,7 @@ async def is_admin(message_or_cq) -> bool:
     client = message._client
     member = await client.get_chat_member(message.chat.id, message.from_user.id)
     return member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]
+
 
 async def is_group_owner(message_or_cq) -> bool:
     if isinstance(message_or_cq, CallbackQuery):
