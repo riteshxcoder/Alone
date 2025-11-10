fcafce780658bd2d3424f87a94da4ca2e5bda6a7
- ♔ 𝐒 𝐇 𝐈 𝐕 𝐀 𝐍 𝐆 🜲 ˹ 𝐎ᴘ ˼﹛🇨🇦﹜≈ 💸 <Shivmis165@gmail.com>
2025-11-10 21:57:42 +0530

    Update Youtube.py

--- AloneMusic/platforms/Youtube.py
+++ AloneMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from py_yt import VideosSearch
-from AarumiMusic.utils.database import is_on_off
-from AarumiMusic.utils.formatters import time_to_seconds
+from AloneMusic.utils.database import is_on_off
+from AloneMusic.utils.formatters import time_to_seconds
 import os
 import glob
 import random
