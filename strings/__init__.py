cec9aa432c158d82eed7a836b8f170aef47f83aa
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-12-10 22:20:38 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -8,36 +8,48 @@
 # All rights reserved.
 
 import os
-from typing import List
-
+import sys
 import yaml
 
-languages = {}
-languages_present = {}
+languages: dict = {}
+languages_present: dict = {}
 
 
 def get_string(lang: str):
     return languages[lang]
 
 
-for filename in os.listdir(r"./strings/langs/"):
+LANG_PATH = "./strings/langs/"
+
+for filename in os.listdir(LANG_PATH):
     if "en" not in languages:
         languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
+            open(os.path.join(LANG_PATH, "en.yml"), encoding="utf8")
         )
         languages_present["en"] = languages["en"]["name"]
-    if filename.endswith(".yml"):
-        language_name = filename[:-4]
-        if language_name == "en":
-            continue
-        languages[language_name] = yaml.safe_load(
-            open(r"./strings/langs/" + filename, encoding="utf8")
-        )
-        for item in languages["en"]:
-            if item not in languages[language_name]:
-                languages[language_name][item] = languages["en"][item]
+
+    if not filename.endswith(".yml"):
+        continue
+
+    language_name = filename[:-4]
+
+    if language_name == "en":
+        continue
+
+    languages[language_name] = yaml.safe_load(
+        open(os.path.join(LANG_PATH, filename), encoding="utf8")
+    )
+
+    for item in languages["en"]:
+        if item not in languages[language_name]:
+            languages[language_name][item] = languages["en"][item]
+
+    # ✅ FIXED: No bare except
     try:
         languages_present[language_name] = languages[language_name]["name"]
-    except:
-        print("There is some issue with the language file inside bot.")
-        exit()
+    except KeyError:
+        print(
+            f"Language file '{filename}' is missing required key: 'name'",
+            file=sys.stderr,
+        )
+        sys.exit(1)
